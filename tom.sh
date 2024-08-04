203248bcdf4db9bd314a66c411b30407347c24e0
hecarch <hecatonchire@disroot.org>
2024-08-04 20:30:56 +0200

    1.0 version

--- tom.sh
+++ tom.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
-#from hecatonchire
+#author : hecatonchire
 ### Hugo implementation in bash
 
+# color variables
+readonly normal="\033[0m"
+readonly red_strong="\033[1;31m"
+readonly white_strong="\033[1;37m"
+readonly green_strong="\033[1;32m"
+
+help_fn() {
+	printf "${red_strong}[+] ${white_strong}Help page for tom usage and option.\n"
+	printf "${green_strong}-h${white_strong} ; ${green_strong}--help\n"
+	printf "${white_strong}		Print this help page.\n"
+	printf "\n"
+	printf "${green_strong}-d${white_strong} ; ${green_strong}--dependencies\n"
+	printf "${white_strong}		Print tom's dependencies.\n"
+	printf "\n"
+	printf "${red_strong}[+] ${white_strong}How to use tom ?\n"
+	printf "${green_strong}->	${white_strong}template.html is the html file from which generated html articles are based. You should write <!-- tomindice --> before your article. It should be in the website root directory.\n"
+	printf "${green_strong}->	${white_strong}index.html is the page where your html article pages are sum-up. If you generate an article with tom, an extract will be automatically added to this file, with a link for your article.\n"
+	printf "${green_strong}->	${white_strong}src/ is the directory where you should put your markdown files.\n"
+	printf "${green_strong}->	${white_strong}articles/ is the directory where html pages are generated.\n"
+}
+
+dep_fn() {
+	printf "${red_strong}[+] ${white_strong}Softwares needed by tom to work.\n"
+	printf "${green_strong}->	${white_strong}pandoc\n"
+}
+
 update_summary() {
-	#créé le content avec le titre du .md, la date et les premières lignes de l'article puis l'ajouter a l'index
+	#generates article fragment based on its markdown file's title, date of generation and the first article lines, then add it to index.html
 	article_name=$(echo "$1" | sed 's/.html//')
 	add_content="<div class=\"sum-article\"><h2><a href="articles/$article_name.html">$article_name - $(date "+%x")</a></h2><p>$(cat src/${article_name}.md | pandoc --from=markdown --to=html | sed 's/<p>//;s/<\/p>//' | sed "/<h[0-9]/d" | head -n 5) [...]</p></div>"
 	echo $add_content > tmp-index.html
@@ -13,7 +39,7 @@ update_summary() {
 
 main() {
 	pandoc src/$1 -o pandoc-tmp.html
-	#insérer dans une copie du template html l'article en html après le keyword
+	#inserts html article just after the keyword "tomindice" in the template.html
 	article_name=$(echo "$1"| sed 's/.md//g')
 	cp template.html articles/$article_name.html
 	sed -i -e '/tomindice/r pandoc-tmp.html' articles/$article_name.html
@@ -23,8 +49,25 @@ main() {
 }
 
 if [[ -z $1 ]]; then
-	echo "Markdown filename must be provided\n"
-	echo "Don't forget to add keyword 'tomindice'"
+	printf "${red_strong}[->	Markdown filename or option must be provided.\n"
+	printf "${green_strong}[?	${white_strong}Type ./tom.sh -h to get some help.\n"
 else
-	main $1
+	case $1 in
+		-h | --help)
+			help_fn
+			;;
+		-d | --dependencies)
+			dep_fn
+			;;
+		-c | --config)
+			conf_fn
+			;;
+		*)
+			if [[ -e "src/$1" ]]; then
+				main $1
+			else
+				echo "Option not understood."
+			fi
+			;;
+	esac
 fi
